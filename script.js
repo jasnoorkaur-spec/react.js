66a154b65d564322f4648acc90685282d5d181ab
Jasnoor Kaur <jasnoorkaur870@gmail.com>
2026-06-11 22:48:16 +0530

    01-folder done

---
+++ script.js
@@ -0,0 +1,12 @@
+// import array from "./app.js"
+// console.log(array); 
+
+// import {arr} from "./app.js"
+// console.log(arr);
+
+// import naam from "./app.js"
+// console.log(naam);
+// import {age} from "./app.js"
+// console.log(age);
+// import {skills} from './app.js'
+// console.log(skills);
