36c0c5d846e81a1e364c2c772c7d281467e38135
Jasnoor Kaur <jasnoorkaur870@gmail.com>
2026-06-13 21:28:36 +0530

    modified

---
+++ 01-folder/src/components/card.jsx
@@ -0,0 +1,11 @@
+function Card()
+{
+    return (
+
+    <div className="Card">
+        <h1>Jasnoor Kaur</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatibus nemo est distinctio dolor cupiditate necessitatibus, impedit, quo corporis obcaecati enim, repellat velit? Officia, consequuntur ex. Distinctio odit eos voluptates?</p>
+    </div> )
+}
+
+export default Card;
