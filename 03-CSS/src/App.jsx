9801d91097d2a5a11f0235102f894134d1df27ec
Jasnoor Kaur <jasnoorkaur870@gmail.com>
2026-06-16 12:47:30 +0530

    modified

--- 03-CSS/src/App.jsx
+++ 03-CSS/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+// import React from 'react'
+// import Header from './components/header/header.jsx'
+// import Button from './components/button/button.jsx'
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Button />
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+
+// Intro to Tailwind CSS
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div className='h-screen w-full bg-gray-900'>
+      
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ 03-CSS/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import styles from "./header.module.css"
+
+// const Header = () => {
+//   return (
+//     <div className={styles.header} >
+//       <h3>Jasnoor</h3>
+//       <button className={styles.btn} >Click Me!</button>
+//     </div>
+//   )
+// }
+
+// export default Header
